560843327423b1f4a2f429c4f355fcca39de63b2
omkaromu183 <omkaromu183@gmail.com>
2024-10-24 18:34:44 +0000

    changed

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -6,87 +6,99 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ColorTween colorTween = ColorTween(
-        begin: Colors.white, end: const Color.fromARGB(255, 110, 178, 241));
+    LinearGradient linearGradient = const LinearGradient(
+      colors: [Colors.red, Colors.blue, Colors.green],
+      begin: AlignmentDirectional.topStart,
+      end: AlignmentDirectional.bottomEnd,
+    );
+
     return Scaffold(
-      body: TweenAnimationBuilder(
-        tween: colorTween,
-        duration: const Duration(milliseconds: 1500),
-        builder: (context, value, child) => Container(
-          color: value,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                const TextField(
-                    decoration: InputDecoration(
-                  constraints: BoxConstraints(maxWidth: 300, maxHeight: 50),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  labelText: 'Email/Contact',
-                )),
-                const SizedBox(
-                  height: 20,
-                ),
-                const TextField(
-                    decoration: InputDecoration(
-                  constraints: BoxConstraints(maxWidth: 300, maxHeight: 50),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  labelText: 'Password',
-                )),
-                const SizedBox(
-                  height: 30,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(100, 40),
-                    backgroundColor: const Color.fromARGB(204, 47, 76, 204),
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: linearGradient,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                controller: TextEditingController(text: 'E-mail/Contact'),
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.blueGrey,
+                    constraints: const BoxConstraints(
+                      maxHeight: 50,
+                      maxWidth: 300,
+                    ),
+                    border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: TextEditingController(text: 'Password'),
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.blueGrey,
+                    constraints: const BoxConstraints(
+                      maxHeight: 50,
+                      maxWidth: 300,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(100, 40),
+                  backgroundColor: const Color.fromARGB(204, 47, 76, 204),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: const Text('Log In'),
                 ),
-                const SizedBox(
-                  height: 10,
+                child: const Text('Log In'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text.rich(
+                TextSpan(
+                  text: 'Don`t have an account? ',
+                  children: [
+                    TextSpan(
+                        text: 'Sign Up',
+                        onEnter: (event) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Signin(),
+                            ),
+                          );
+                        },
+                        style: TextStyle(
+                          color: Colors.blue.shade900,
+                          decoration: TextDecoration.underline,
+                        ))
+                  ],
                 ),
-                Text.rich(
-                  TextSpan(
-                    text: 'Don`t have an account? ',
-                    children: [
-                      TextSpan(
-                          text: 'Sign Up',
-                          onEnter: (event) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const SignUp(),
-                              ),
-                            );
-                          },
-                          style: TextStyle(
-                            color: Colors.blue.shade900,
-                            decoration: TextDecoration.underline,
-                          ))
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -18,216 +18,148 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: const SignUp(),
+      home: const Signin(),
     );
   }
 }
 
-class SignUp extends StatelessWidget {
-  const SignUp({super.key});
+class Signin extends StatelessWidget {
+  const Signin({super.key});
 
   @override
   Widget build(BuildContext context) {
-    ColorTween colorTween = ColorTween(
-        begin: Colors.white, end: const Color.fromARGB(255, 47, 204, 86));
-
+    LinearGradient linearGradient = const LinearGradient(
+      colors: [Colors.red, Colors.blue, Colors.green],
+      begin: AlignmentDirectional.topStart,
+      end: AlignmentDirectional.bottomEnd,
+    );
     return Scaffold(
-      body: TweenAnimationBuilder(
-        tween: colorTween,
-        duration: const Duration(milliseconds: 1500),
-        builder: (context, value, child) => Container(
-          color: value,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 30,
-                ),
-                const TextField(
-                    decoration: InputDecoration(
-                  constraints: BoxConstraints(maxWidth: 300, maxHeight: 50),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  labelText: 'Name',
-                )),
-                const SizedBox(
-                  height: 20,
-                ),
-                const TextField(
-                    decoration: InputDecoration(
-                  constraints: BoxConstraints(maxWidth: 300, maxHeight: 50),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  labelText: 'Contact',
-                )),
-                const SizedBox(
-                  height: 20,
-                ),
-                const TextField(
-                    decoration: InputDecoration(
-                  constraints: BoxConstraints(maxWidth: 300, maxHeight: 50),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  labelText: 'E-mail',
-                )),
-                const SizedBox(
-                  height: 20,
-                ),
-                const TextField(
-                    decoration: InputDecoration(
-                  constraints: BoxConstraints(maxWidth: 300, maxHeight: 50),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  labelText: 'Password',
-                )),
-                const SizedBox(
-                  height: 30,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(100, 40),
-                    backgroundColor: const Color.fromARGB(204, 47, 76, 204),
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: linearGradient,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                controller: TextEditingController(text: 'Name'),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.blueGrey,
+                    constraints: const BoxConstraints(
+                      maxHeight: 50,
+                      maxWidth: 300,
+                    ),
+                    border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: TextEditingController(text: 'Contact'),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.blueGrey,
+                    constraints: const BoxConstraints(
+                      maxHeight: 50,
+                      maxWidth: 300,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: TextEditingController(text: 'E-mail'),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.blueGrey,
+                    constraints: const BoxConstraints(
+                      maxHeight: 50,
+                      maxWidth: 300,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: TextEditingController(text: 'Password'),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.blueGrey,
+                    constraints: const BoxConstraints(
+                      maxHeight: 50,
+                      maxWidth: 300,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(100, 40),
+                  backgroundColor: const Color.fromARGB(204, 47, 76, 204),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: const Text('Sign Up'),
                 ),
-                const SizedBox(
-                  height: 10,
+                child: const Text('Sign Up'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text.rich(
+                TextSpan(
+                  text: 'Already have an account? ',
+                  children: [
+                    TextSpan(
+                        text: 'Login',
+                        onEnter: (event) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Login()),
+                          );
+                        },
+                        style: TextStyle(
+                          color: Colors.blue.shade900,
+                          decoration: TextDecoration.underline,
+                        ))
+                  ],
                 ),
-                Text.rich(
-                  TextSpan(
-                    text: 'Already have an account? ',
-                    children: [
-                      TextSpan(
-                          text: 'Login',
-                          onEnter: (event) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Login()),
-                            );
-                          },
-                          style: TextStyle(
-                            color: Colors.blue.shade900,
-                            decoration: TextDecoration.underline,
-                          ))
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
